6c6254f0e30bbf7f687ca0f3e1d099fac7fede4d
you-ng22 <97034762+you-ng22@users.noreply.github.com>
2022-11-08 10:21:15 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 45.68 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,14 @@
+def solution(n, m):
+    gcd = 0
+    lcm = 0
+    #최대공약수
+    for i in range(min(n,m), 0, -1):
+        if n % i == 0 and m % i == 0:
+            gcd += i
+            break
+    #최소공배수
+    for j in range(max(n,m), (n*m)+1):
+        if j % n == 0 and j % m == 0:
+            lcm += j
+            break
+    return gcd, lcm
